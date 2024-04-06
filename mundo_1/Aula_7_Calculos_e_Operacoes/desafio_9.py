7521b916d27bae8c7f5f2194e70269dd11729101
Reinaldo Duarte <62804743+reicd@users.noreply.github.com>
2024-04-06 15:16:02 -0300

    Update desafio_9.py
    Fiz os números de 2 a 9 sem loop

--- mundo_1/Aula_7_Calculos_e_Operacoes/desafio_9.py
+++ mundo_1/Aula_7_Calculos_e_Operacoes/desafio_9.py
@@ -5,5 +5,15 @@ n = int(input("Digite um número: \n"))
 print("Tabuada do {}:".format(n))
 
 print("{} x {} = {}".format(n, 1, (n * 1)))
+print("{} x {} = {}".format(n, 1, (n * 2)))
+print("{} x {} = {}".format(n, 1, (n * 3)))
+print("{} x {} = {}".format(n, 1, (n * 4)))
+print("{} x {} = {}".format(n, 1, (n * 5)))
+print("{} x {} = {}".format(n, 1, (n * 6)))
+print("{} x {} = {}".format(n, 1, (n * 7)))
+print("{} x {} = {}".format(n, 1, (n * 8)))
+print("{} x {} = {}".format(n, 1, (n * 9)))
+print("{} x {} = {}".format(n, 1, (n * 10)))
+
 
 # Sem loop o código é repetitivo
